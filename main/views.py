6ad46138d5da8db7d211a8247fa65ab0c05acf59
Prince Owusu Asante <owusup484@gmail.com>
2025-11-17 14:05:30 +0000

    'update'

--- main/views.py
+++ main/views.py
@@ -10,7 +10,6 @@ from datetime import datetime, timedelta
 from django.utils import timezone
 from rest_framework.permissions import AllowAny
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.response import Response
 from rest_framework import status
 import logging
 
