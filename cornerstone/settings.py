48ffab878c24fd219dd4be7c086b2d8c5a4c14a6
Prince Owusu Asante <owusup484@gmail.com>
2025-11-24 02:30:07 +0000

    ''

--- cornerstone/settings.py
+++ cornerstone/settings.py
@@ -110,7 +110,9 @@ DATABASES = {
 # EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
-SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
+SENDGRID_API_KEY = (
+    "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"
+)
 
 SITE_NAME = "Cornerstone Development And Construction"
 
